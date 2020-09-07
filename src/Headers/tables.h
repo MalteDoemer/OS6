c4f646b354b5e473e53d7987b257e1403260b5dc
Malte Dömer <maltedoemer@gmail.com>
2020-09-07 21:32:02 +0200

    idt entrys with macros

--- src/Headers/tables.h
+++ src/Headers/tables.h
@@ -28,9 +28,14 @@ typedef struct _GDT_ENTRY
     byte base_high;
 } GDT_ENTRY;
 
-extern IDT_ENTRY idt[256];
+typedef struct _INT_SUB
+{
+    byte bytes[11];
+} INT_SUB;
 
+extern IDT_ENTRY idt[256];
 extern GDT_ENTRY gdt[3];
+extern INT_SUB isr_table[256];
 
 void init_idt();
 void init_gdt();
@@ -38,33 +43,7 @@ void init_gdt();
 void load_idt(word limit, qword base);
 void load_gdt(word limit, qword base);
 
-void set_idt_entry(int index, void (*int_sub)(), word selector, byte attributes);
+void set_idt_entry(int index, void *int_sub, word selector, byte attributes);
 void set_gdt_entry(int index, dword base, dword limit, byte access, byte flags);
-
-void exception_00();
-void exception_01();
-void exception_02();
-void exception_03();
-void exception_04();
-void exception_05();
-void exception_06();
-void exception_07();
-void exception_08();
-void exception_09();
-void exception_10();
-void exception_11();
-void exception_12();
-void exception_13();
-void exception_14();
-void exception_15();
-void exception_16();
-void exception_17();
-void exception_18();
-void exception_19();
-
-void exception_gate(); // default exception gate
-void interrupt_gate(); // default interrupt gate
-
-void cascade_irq();
 
 #endif // #ifndef TABLES_H
--- src/Kernel/tables.c
+++ src/Kernel/tables.c
@@ -21,43 +21,16 @@ void init_idt()
     outb(0x21, MASTER_IRQS); // Mask all unused master irq's
     outb(0xA1, SLAVE_IRQS);  // Mask all unused slave irq's
 
-    // set all defined cpu excepions
-    set_idt_entry(0, exception_00, 0x08, 0x8F);
-    set_idt_entry(1, exception_01, 0x08, 0x8F);
-    set_idt_entry(2, exception_02, 0x08, 0x8F);
-    set_idt_entry(3, exception_03, 0x08, 0x8F);
-    set_idt_entry(4, exception_04, 0x08, 0x8F);
-    set_idt_entry(5, exception_05, 0x08, 0x8F);
-    set_idt_entry(6, exception_06, 0x08, 0x8F);
-    set_idt_entry(7, exception_07, 0x08, 0x8F);
-    set_idt_entry(8, exception_08, 0x08, 0x8F);
-    set_idt_entry(9, exception_09, 0x08, 0x8F);
-    set_idt_entry(10, exception_10, 0x08, 0x8F);
-    set_idt_entry(11, exception_11, 0x08, 0x8F);
-    set_idt_entry(12, exception_12, 0x08, 0x8F);
-    set_idt_entry(13, exception_13, 0x08, 0x8F);
-    set_idt_entry(14, exception_14, 0x08, 0x8F);
-    set_idt_entry(15, exception_15, 0x08, 0x8F);
-    set_idt_entry(16, exception_16, 0x08, 0x8F);
-    set_idt_entry(17, exception_17, 0x08, 0x8F);
-    set_idt_entry(18, exception_18, 0x08, 0x8F);
-    set_idt_entry(19, exception_19, 0x08, 0x8F);
+    for (int i = 0; i < 32; i++)
+        set_idt_entry(i, isr_table + i, 0x08, 0x8F);
 
-    // fill out rest of the exceptions with a default handler
-    for (int i = 20; i < 32; i++)
-        set_idt_entry(i, exception_gate, 0x08, 0x8F);
-
-    // fill out rest of the interrupts with a default handler
     for (int i = 32; i < 256; i++)
-        set_idt_entry(i, interrupt_gate, 0x08, 0x8E);
-
-    // set up master->slave cascade irq
-    set_idt_entry(0x22, cascade_irq, 0x08, 0x8E);
+        set_idt_entry(i, isr_table + i, 0x08, 0x8E);
 
     load_idt(sizeof(idt) - 1, (qword)idt);
 }
 
-void set_idt_entry(int index, void (*int_sub)(), word selector, byte attributes)
+void set_idt_entry(int index, void *int_sub, word selector, byte attributes)
 {
     qword addr = (qword)int_sub;
 
